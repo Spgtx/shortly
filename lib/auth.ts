0570f178d6dfa90a04117186aa85027446a3a7c0
Spgtx <speerexgtx@gmail.com>
2025-08-24 13:29:25 +0300

    ...

--- lib/auth.ts
+++ lib/auth.ts
@@ -13,13 +13,14 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
-      },
-    }),
+    async jwt({ token, user }) {
+      if (user) token.id = user.id;
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) session.user.id = token.id as string;
+      return session;
+    },
   },
   secret: env.NEXTAUTH_SECRET,
 };
